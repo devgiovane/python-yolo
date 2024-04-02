36cf7274a1880729f4baebcde1a114fe2fae9f78
Giovane Santos Silva <giovanesantos1999@gmail.com>
2024-04-02 10:05:06 -0300

    add: train with other dataset

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import os
 from ultralytics import YOLO
 
 model_n = YOLO('yolov8n.pt')
-results_n = model_n.train(
+model_n.train(
     data=os.path.abspath('./dataset/data.yaml'),
     epochs=150,
     cache='ram',
@@ -10,17 +10,27 @@ results_n = model_n.train(
     imgsz=640,
     degrees=+180
 )
-
 model_n.export(format='ONNX')
 
 model_s = YOLO('yolov8s.pt')
-results_s = model_s.train(
+model_s.train(
     data=os.path.abspath('./dataset2/data.yaml'),
-    epochs=150,
+    epochs=120,
     cache='ram',
     batch=-1,
     imgsz=640,
     degrees=-180
 )
-
 model_s.export(format='ONNX')
+
+
+model_b = YOLO('./runs/detect/train/weights/bets.pt')
+model_b.train(
+    data=os.path.abspath('./dataset2/data.yaml'),
+    epochs=120,
+    cache='ram',
+    batch=-1,
+    imgsz=640,
+    degrees=-180
+)
+model_b.export(format='ONNX')
